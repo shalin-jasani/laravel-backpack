89ed53b6ebd893bc39860148f709225b7721149a
shalin-jasani <shalin.jasani@arsenaltech.com>
2019-01-01 19:05:34 +0530

    - react crud demo

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::all();
+        return response()->json($products);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $product = new Product([
+            'title' => $request->get('title'),
+            'body' => $request->get('body')
+        ]);
+        $product->save();
+
+
+        return response()->json('Product Added Successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        return response()->json($product);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        $product->title = $request->get('title');
+        $product->body = $request->get('body');
+        $product->save();
+
+        return response()->json('Product Updated Successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return response()->json('Product Deleted Successfully.');
+    }
+}
---
+++ resources/js/components/MyGlobleSetting.js
@@ -0,0 +1,6 @@
+class MyGlobleSetting {
+  constructor() {
+    this.url = 'http://laravel-backpack.local';
+  }
+}
+export default (new MyGlobleSetting);
